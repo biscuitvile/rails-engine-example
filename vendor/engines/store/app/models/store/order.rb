6ae58d9fb09fd596fe59beb446eb6414bfdeaa46
Andy Borsz <andy.borsz@gmail.com>
2012-04-01 17:28:32 -0400

    Add store order validations

--- vendor/engines/store/app/models/store/order.rb
+++ vendor/engines/store/app/models/store/order.rb
@@ -2,5 +2,9 @@ module Store
   class Order < ActiveRecord::Base
     belongs_to :product, :class_name => "ProductManager::Product"
     belongs_to :user
+
+    validates :product,  :presence => true
+    validates :quantity, :presence => true
+    validates :user,     :presence => true
   end
 end
