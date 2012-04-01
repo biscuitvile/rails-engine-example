fe504ee16f67df693b32dc2b357ee5c6c830251a
Andy Borsz <andy.borsz@gmail.com>
2012-03-31 20:13:05 -0400

    Install devise

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,10 @@ module EngineTutorial
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # force application to not access the DB
+    # or load models when precompiling your assets
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ EngineTutorial::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # acion mailer default url
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
