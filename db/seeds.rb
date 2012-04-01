f6594f6da0e5613a399452e4d45e14d89ecfb697
Andy Borsz <andy.borsz@gmail.com>
2012-04-01 17:22:44 -0400

    Add seeds file and simplified login

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+User.create(
+  :email => 'example.user@example.com',
+  :display_name => 'Example User',
+  :password => 'password',
+  :password_confirmation => 'password'
+)
+
+ProductManager::Product.destroy_all
+ProductManager::Product.create(
+  :name => 'Glue',
+  :price => '0.99'
+)
+
+ProductManager::Product.create(
+  :name => 'Rat traps',
+  :price => '1.59'
+)
+
+ProductManager::Product.create(
+  :name => 'Settlers of Catan',
+  :price => '19.99'
+)
+
+ProductManager::Product.create(
+  :name => 'Dangerous-looking Yak',
+  :price => '299.99'
+)
+
+Store::Order.destroy_all
+
+o1 = Store::Order.new(
+  :product_id => ProductManager::Product.first.id,
+  :quantity => 2
+)
+
+o1.user_id = User.first.id
+o1.save
+
+o2 = Store::Order.new(
+  :product_id => ProductManager::Product.all[1].id,
+  :quantity => 3,
+)
+
+o2.user_id = User.first.id
+o2.save
