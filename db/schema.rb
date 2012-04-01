1ef52c029061ba2f1f972ca9ae3855e2e5882b4e
Andy Borsz <andy.borsz@gmail.com>
2012-03-31 23:43:48 -0400

    Add display name to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120401021913) do
+ActiveRecord::Schema.define(:version => 20120401033941) do
 
   create_table "product_manager_products", :force => true do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20120401021913) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "display_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
