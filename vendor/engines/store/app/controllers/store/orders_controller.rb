731a6586cc2893bd8ddd1255d9e15ff1dd463142
Andy Borsz <andy.borsz@gmail.com>
2012-04-04 08:06:42 -0400

    whitespace

--- vendor/engines/store/app/controllers/store/orders_controller.rb
+++ vendor/engines/store/app/controllers/store/orders_controller.rb
@@ -4,45 +4,45 @@ module Store
     # GET /orders.json
     def index
       @orders = Order.all
-  
+
       respond_to do |format|
         format.html # index.html.erb
         format.json { render json: @orders }
       end
     end
-  
+
     # GET /orders/1
     # GET /orders/1.json
     def show
       @order = Order.find(params[:id])
-  
+
       respond_to do |format|
         format.html # show.html.erb
         format.json { render json: @order }
       end
     end
-  
+
     # GET /orders/new
     # GET /orders/new.json
     def new
       @order = Order.new
-  
+
       respond_to do |format|
         format.html # new.html.erb
         format.json { render json: @order }
       end
     end
-  
+
     # GET /orders/1/edit
     def edit
       @order = Order.find(params[:id])
     end
-  
+
     # POST /orders
     # POST /orders.json
     def create
       @order = Order.new(params[:order])
-  
+
       respond_to do |format|
         if @order.save
           format.html { redirect_to @order, notice: 'Order was successfully created.' }
@@ -53,12 +53,12 @@ module Store
         end
       end
     end
-  
+
     # PUT /orders/1
     # PUT /orders/1.json
     def update
       @order = Order.find(params[:id])
-  
+
       respond_to do |format|
         if @order.update_attributes(params[:order])
           format.html { redirect_to @order, notice: 'Order was successfully updated.' }
@@ -69,13 +69,13 @@ module Store
         end
       end
     end
-  
+
     # DELETE /orders/1
     # DELETE /orders/1.json
     def destroy
       @order = Order.find(params[:id])
       @order.destroy
-  
+
       respond_to do |format|
         format.html { redirect_to orders_url }
         format.json { head :no_content }
