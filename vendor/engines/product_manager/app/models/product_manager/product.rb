ec158e06eacb8031f88e2c4a75dc480e736ab8af
Andy Borsz <andy.borsz@gmail.com>
2012-04-04 07:39:04 -0400

    Add dependent destroy relation between product and orders

--- vendor/engines/product_manager/app/models/product_manager/product.rb
+++ vendor/engines/product_manager/app/models/product_manager/product.rb
@@ -1,5 +1,9 @@
 module ProductManager
   class Product < ActiveRecord::Base
+    has_many :orders,
+              :class_name => 'Store::Order',
+              :dependent => :destroy
+
     validates :name,  :presence => true
     validates :price, :presence => true
   end
